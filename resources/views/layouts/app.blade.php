c4a80f7a282a27a8863ee36749b05640d0c2b8a0
Simon Kronstein <skronstein7@gmail.com>
2022-03-30 07:10:31 -0700

    add tailwindcss and sass

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>Burnout 2 Cheat Rankings</title>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 </head>
 <body>
     @yield('content')
