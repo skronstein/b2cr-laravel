83f35c8028a6dc6c2f9f98c245668cc59721a39a
Simon Kronstein <skronstein7@gmail.com>
2022-03-31 06:28:03 -0700

    add navbar and Tracks div to /tracks

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,20 @@
     <title>Burnout 2 Cheat Rankings</title>
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 </head>
-<body>
+<body class="bg-gray-200">
+    <nav class="p-6 bg-white flex justify-between mb-6">
+        <ul class="flex items-center">
+            <li>
+                <a href="" class="p-3">Home</a>
+            </li>
+            <li>
+                <a href="" class="p-3">Dashboard</a>
+            </li>
+            <li>
+                <a href="" class="p-3">Posts</a>
+            </li>
+        </ul>
+    </nav>
     @yield('content')
 </body>
 </html>
