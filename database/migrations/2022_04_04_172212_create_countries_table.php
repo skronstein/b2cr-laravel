840814d2fa760706d19061899675ffea6d119728
Simon Kronstein <skronstein7@gmail.com>
2022-04-04 17:45:48 -0700

    create database migrations

--- database/migrations/2022_04_04_172212_create_countries_table.php
+++ database/migrations/2022_04_04_172212_create_countries_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateCountriesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,12 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('countries', function (Blueprint $table) {
             $table->id();
-            $table->string('username');
-            $table->string('password_hash');
-            $table->rememberToken();
+            $table->string('name');
+            $table->string('name_url');
+            $table->string('abr');
+            $table->string('iso');
             $table->timestamps();
         });
     }
@@ -29,6 +30,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('countries');
     }
 }
---
+++ database/migrations/2022_04_04_172413_create_players_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePlayersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('players', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('name_url');
+            $table->unsignedBigInteger('country');
+            $table->foreign('country')->references('id')->on('countries')->onDelete('cascade');
+            $table->string('notes');
+            $table->string('wcr_channel');
+            $table->string('personnal_channel');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('players');
+    }
+}
