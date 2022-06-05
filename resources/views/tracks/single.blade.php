00e14ac0e3d60b1aa140b3cd7ff7c513caff9bf5
Simon Kronstein <skronstein7@gmail.com>
2022-06-05 07:11:56 -0700

    also show date_acheived, car, system, proof

--- resources/views/tracks/single.blade.php
+++ resources/views/tracks/single.blade.php
@@ -5,8 +5,9 @@
         <div class="w-8/12 bg-white p-6 rounded-lg">
             {{--$heading--}}<br>
             @foreach($records as $record)
-                {{--//show count(rank)--}}<br>
-                {{$record->score}} {{$record->player_id}}
+                {{$loop->iteration}} {{$record->score}} {{$record->player_id}}
+                {{$record->car}} {{$record->date_acheived}} {{$record->system}} {{$record->proof}}
+                <br>
             @endforeach
         </div>
     </div>
